4b96ff8cd1fbda7b72f48b707c06fe7d156370f5
Christine Lee <59518411+L-Christine@users.noreply.github.com>
2023-10-17 14:54:25 -0400

    Add files via upload

---
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if (input == 0) {
+                if (numbers.isEmpty()) {
+                    System.out.println("Cannot calculate the average");
+                    break;
+                }
+                int sum = 0;
+                for (Integer each : numbers) {
+                    sum += each;
+                }
+                double avg = 1.0 * sum / numbers.size();
+                System.out.println(avg);
+                break;
+            }
+            
+            if (input > 0) {
+                numbers.add(input);
+            }
+        }
+    }
+}
