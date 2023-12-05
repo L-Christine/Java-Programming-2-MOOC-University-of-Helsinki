eac9b91cb2eb1a5e2b9257abac14a53477961110
Christine Lee <59518411+L-Christine@users.noreply.github.com>
2023-12-05 02:42:55 -0500

    Add files via upload

---
+++ part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Book> books = new ArrayList<>();
+        while (true) {
+            System.out.println("Input the name of the book, empty stops: ");
+            String title = scanner.nextLine();
+            if (title.isEmpty()) {
+                break;
+            }
+            System.out.println("Input the age recommendation: ");
+            int age = Integer.valueOf(scanner.nextLine());
+            books.add(new Book(title, age));
+        }
+        System.out.println(books.size() + " books in total.");
+        System.out.println("Books:");
+        Comparator<Book> comparator = Comparator.comparing(Book::getAge).thenComparing(Book::getTitle);
+        Collections.sort(books, comparator);
+        for (Book book: books) {
+            System.out.println(book);
+        }
+    }
+
+}
