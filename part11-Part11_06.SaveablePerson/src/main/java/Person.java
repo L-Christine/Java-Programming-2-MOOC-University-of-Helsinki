7bc7f67fb672e0397024655adbdec83fdf3f9d10
Christine Lee <59518411+L-Christine@users.noreply.github.com>
2023-12-15 08:23:24 -0500

    Add files via upload

---
+++ part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,21 @@
+
+public class Person implements Saveable {
+    private String name;
+    private String address;
+    
+    public Person() {
+        
+    }
+    
+    public void save() {
+        
+    }
+    
+    public void delete() {
+        
+    }
+    
+    public void load(String address) {
+        
+    }
+}
